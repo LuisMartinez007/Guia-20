1cd096691cc739322d44e2d2ca7cae212dd37f56
Luis <luis.martinezlopez@unicolombo.edu.co>
2024-04-23 15:45:21 -0500

    Dialogos

---
+++ Guia#20/src/guias/gui/principal/Main.java
@@ -0,0 +1,22 @@
+
+package guias.gui.principal;
+
+import guias.gui.views.Window;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Main {
+    
+public static void main(String[]args){
+
+Window ventana = new Window();
+
+ventana.setLocationRelativeTo(null);
+ventana.setVisible(true);
+
+    
+} 
+    
+}
